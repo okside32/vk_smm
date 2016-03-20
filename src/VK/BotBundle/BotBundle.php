c248a3cfc8d952de1611e048d45010212d91f236
okside32 <okside32@gmail.com>
2016-03-20 21:25:02 +0200

    1

---
+++ src/VK/BotBundle/BotBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace VK\BotBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BotBundle extends Bundle
+{
+}
---
+++ src/VK/BotBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace VK\BotBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('BotBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
